aa66acc9d4cb263767af80dbd1259bd1630c9bb4
Jonny248 <Jonathan.S94@web.de>
2020-10-02 12:47:56 +0200

    Create Taster_als_Schalter_2.ino

---
+++ Taster_als_Schalter_2/Taster_als_Schalter_2.ino
@@ -0,0 +1,41 @@
+#include <OneButton.h>
+
+OneButton taster(8, true);
+
+#define LED 2   //LED an Pin 2
+#define Taster 8  //Pulldown
+
+boolean statusLED;
+
+
+void setup() 
+{
+  pinMode(2, OUTPUT);  //LED an Pin 2
+
+  taster.attachClick(Funktion_Taster);
+} 
+
+void loop()
+{
+  taster.tick();
+
+   
+  delay(10);  
+}
+
+void Funktion_Taster()
+{
+   if(digitalRead(Taster) == 1)
+  {
+    if(statusLED == 1)
+    {
+      digitalWrite(LED, LOW);
+      statusLED = 0;
+    }
+    else
+    {
+      digitalWrite(LED, HIGH);
+      statusLED = 1;
+    }
+  }
+}
