22fc6567b12ecdb00033bbdc88b6efa09e8ba35c
Jonny248 <Jonathan.S94@web.de>
2020-10-06 13:16:04 +0200

    Update Taster_als_Schalter_2.ino
    Ausgabe des LED Signals aus der Funktion geholt und in den Loop geschrieben

--- Taster_als_Schalter_2/Taster_als_Schalter_2.ino
+++ Taster_als_Schalter_2/Taster_als_Schalter_2.ino
@@ -1,39 +1,36 @@
-#include <OneButton.h>
 
-OneButton taster(8, true);
+#include <OneButton.h>
 
 #define LED 2   //LED an Pin 2
 #define Taster 8  //Pulldown
 
 boolean statusLED;
 
+OneButton taster(Taster, false);
+
+
+
+
 
 void setup()
 {
   pinMode(2, OUTPUT);  //LED an Pin 2
 
-  taster.attachClick(Funktion_Taster);
+  taster.attachClick(Funktion_Taster);  // Wenn das Programm ein Signal am Taster Eingang wahrnimmt, springt er zu der Funktion
 }
 
 void loop()
 {
-  taster.tick();
+  taster.tick();  //Abfrage des Tasters
 
 
   delay(10);
+
+digitalWrite(LED, statusLED);  //Status des LED Merkers/der LED Status ins digitalWrite einfügen und ausgeben
 }
 
 void Funktion_Taster()
 
 {
-  if (statusLED == 1)
-  {
-    digitalWrite(LED, LOW);
-    statusLED = 0;
-  }
-  else
-  {
-    digitalWrite(LED, HIGH);
-    statusLED = 1;
-  }
+  statusLED = !statusLED;   //Invertierung der statusLED   
 }
